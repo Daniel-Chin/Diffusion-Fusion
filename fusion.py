8782a839353b93456b55ae34f88a2f40eedc1292
Daniel Chin <daniel.chin@nyu.edu>
2022-10-03 13:48:01 +0400

    a

--- fusion.py
+++ fusion.py
@@ -1,3 +1,10 @@
+'''
+Subtracting horse seems useful!?!?
+pixel-level constraints
+    symmetry
+    横看成岭侧成峰
+'''
+
 from contextlib import nullcontext
 
 from transformers import CLIPTextModel, CLIPTokenizer
@@ -6,10 +13,14 @@ from diffusers import AutoencoderKL, UNet2DConditionModel, PNDMScheduler
 from diffusers import LMSDiscreteScheduler
 import torch
 from tqdm.auto import tqdm
-from PIL import Image
+from matplotlib import pyplot as plt
+from winsound import Beep
 logging.set_verbosity_error()
 
 from shared import *
+
+LADDER_LEN = 9
+LADDER = torch.linspace(0, 1, LADDER_LEN)
 
 generator = torch.Generator(DEVICE_STR).manual_seed(2333)
 tokenizer: CLIPTokenizer = CLIPTokenizer.from_pretrained(
@@ -40,11 +51,11 @@ def main():
     prompt_pair = [
         "a photo of a person doing a handstand on a horse", 
         "a photo of an astronaut riding a horse on mars", 
+        # "a photo of a horse", 
     ]
     num_inference_steps = 20
     guidance_scale = 7.5
     width, height = 512, 512
-    batch_size = 1
     
     print('Encoding text...')
     text_input_pair = [tokenizer(
@@ -57,7 +68,7 @@ def main():
     )[0] for x in text_input_pair]
 
     uncond_input = tokenizer(
-        [""] * batch_size, padding="max_length", 
+        [""], padding="max_length", 
         max_length=tokenizer.model_max_length, 
         return_tensors="pt", 
     )
@@ -65,60 +76,95 @@ def main():
         uncond_input.input_ids.to(DEVICE)
     )[0]
 
-    text_embeddings = torch.cat([
-        uncond_embeddings, *text_embeddings_pair, 
-    ])
-
     print('Sampling latent...')
     latents = torch.randn((
-        batch_size, unet.in_channels, height // 8, width // 8,
+        1, unet.in_channels, height // 8, width // 8,
     ), generator=generator)
     latents = latents.to(DEVICE)
     scheduler.set_timesteps(num_inference_steps)
 
     latents = latents * scheduler.sigmas[0]
+    latents = torch.cat([latents] * LADDER_LEN)
 
     with torch.autocast(DEVICE_STR) if (
         HAS_CUDA or torch.__version__ > '1.10.0'
     ) else nullcontext():
         for i, t in tqdm([*enumerate(scheduler.timesteps)], 'denoising'):
             latents = oneStep(
-                i, t, latents, guidance_scale, text_embeddings, 
+                i, t, latents, guidance_scale, 
+                uncond_embeddings, text_embeddings_pair,  
             )
 
     print('Decoding image...')
     latents = 1 / 0.18215 * latents
-    image = vae.decode(latents).sample
+    fusion_images = toImg(vae.decode(latents).sample)
+    interp_images = torch.zeros_like(fusion_images)
+    for i, k in enumerate(LADDER):
+        interp_images[i, :, :, :] = toImg(vae.decode(
+            latents[0,  :, :, :] * (1 - k) +
+            latents[-1, :, :, :] * k
+        ).sample)
 
-    image = (image / 2 + 0.5).clamp(0, 1)
-    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
-    images = (image * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-    # pil_images[0].save(prompt[0].replace(' ', '_') + '.png')
-    pil_images[0].save('1.png')
+    fig, axes = plt.subplots(2, LADDER_LEN)
+    for row_i, method_name in enumerate(('fusion', 'interp')):
+        axes[row_i][LADDER_LEN // 2].set_title(method_name)
+        for col_i in range(LADDER_LEN):
+            ax = axes[row_i][col_i]
+            img = locals()[method_name + '_images'][col_i, :, :, :]
+            ax.imshow(img)
+    Beep(330, 2)
+    plt.show()
+    from console import console
+    console({**globals(), **locals()})
+
+def toImg(x: torch.Tensor):
+    x = (x / 2 + 0.5).clamp(0, 1)
+    x = x.cpu().permute(0, 2, 3, 1).numpy()
+    x = (x * 255).round().astype("uint8")
 
 def oneStep(
-    i, t, latents, guidance_scale, text_embeddings, 
+    i, t, latents, guidance_scale, 
+    uncond_embeddings, text_embeddings_pair,  
 ):
-    # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
-    latent_model_input = torch.cat([latents] * 2)
+    latents_expand = torch.cat([latents] * 3)
     sigma = scheduler.sigmas[i]
-    latent_model_input = latent_model_input / (
+    latents_expand = latents_expand / (
         (sigma**2 + 1) ** 0.5
     )
 
     # predict the noise residual
+    text_embeddings = torch.cat(
+        [uncond_embeddings] * LADDER_LEN + 
+        [text_embeddings_pair[0]] * LADDER_LEN + 
+        [text_embeddings_pair[1]] * LADDER_LEN
+    )
     noise_pred: torch.Tensor = unet(
-        latent_model_input, t, 
+        latents_expand, t, 
         encoder_hidden_states=text_embeddings, 
     ).sample
 
     # perform guidance
-    noise_pred_uncond, *noise_pred_text_pair = noise_pred.chunk(3)
-    guides = [x - noise_pred_uncond for x in noise_pred_text_pair]
-    noise_pred = noise_pred_uncond + guidance_scale * torch.stack(
-        guides, dim=0, 
-    ).sum(dim=0) # / 2 ** .5
+    g0 = (
+        noise_pred[1 * LADDER_LEN : 2 * LADDER_LEN, :, :, :] - 
+        noise_pred[0 * LADDER_LEN : 1 * LADDER_LEN, :, :, :]
+    )
+    g1 = (
+        noise_pred[2 * LADDER_LEN : 3 * LADDER_LEN, :, :, :] - 
+        noise_pred[0 * LADDER_LEN : 1 * LADDER_LEN, :, :, :]
+    )
+    # gn = nn - noise_pred_uncond
+    # noise_pred = noise_pred_uncond + (g0 + g1 - gn) * guidance_scale
+    g = g0 + g1
+    my_scale = max(g0.norm(), g1.norm()) / g.norm()
+    noise_pred = (
+        noise_pred[0 * LADDER_LEN : 1 * LADDER_LEN] 
+        + g * guidance_scale * my_scale
+    )
+    # print('\n', 
+    #     g0.norm().item(), 
+    #     g1.norm().item(), 
+    #     (g0 + g1).norm().item() / 2**.5, 
+    # )
 
     # compute the previous noisy sample x_t -> x_t-1
     return scheduler.step(noise_pred, i, latents).prev_sample
